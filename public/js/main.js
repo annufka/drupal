821db1e36c94b733f6c01616460ff8ef0bceaf88
Didukh <Ganna.Didukh@ua.nestle.com>
2022-02-14 13:55:04 +0200

    работает, но без пока localStorage

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,39 @@
 "use strict";
 
+// пусть будет
 let listToDo = [];
 
 const saveButton = document.getElementById("btn-submit");
 
 if (saveButton) { saveButton.addEventListener("click", addItemToList) };
 
-
 function addItemToList() {
     let user_input = document.getElementById("user-input");
     listToDo.push(user_input.value);
     addItemToPage(user_input.value);
+    user_input.value = "";
 }
 
 function addItemToPage(lastItem) {
     let newLi = document.createElement('li');
-    newLi.innerHTML = `<input class="form-check-input" type="checkbox" value="" id="flexCheckIndeterminate">
+    newLi.innerHTML = `<input class="form-check-input done" type="checkbox" value="" id="done${lastItem}">
                         ${lastItem}
-                        <button id="btn-delete" class="btn"><i class="fa fa-trash-o" style="font-size:1pem"></i></button>`
-    document.getElementById('list-will-to-do').appendChild(newLi);
+                        <button id="delete${lastItem}" class="btn btn-delete"><i class="fa fa-trash-o" style="font-size:1pem"></i></button>`
+    document.getElementById('list-will-do').appendChild(newLi);
 
-    // const deleteButton = document.getElementById("btn-delete");
-    // if (newLi) { newLi.addEventListener("click", deleteItem) };
-    newLi.addEventListener("click", deleteItem);
+    document.getElementById(`delete${lastItem}`).addEventListener("click", deleteItem);
+    document.getElementById(`done${lastItem}`).addEventListener("click", doneItem);
 }
 
+function deleteItem(clickedBtn) {
+    clickedBtn.srcElement.closest('li').remove();
+}
 
-function deleteItem() {
-    let deleteBtn = document.getElementById("btn-delete");
-    deleteBtn.parentElement.remove();
-    // deleteBtn.closest('li').remove();
+function doneItem(clickedCheckBox) {
+    let newLi = document.createElement('li');
+    let lastItem = clickedCheckBox.srcElement.closest('li').innerText;
+    newLi.innerHTML = `<input class="form-check-input done" type="checkbox" value="" id="done${lastItem}" checked disabled>
+                        ${lastItem}`
+    document.getElementById('list-done').appendChild(newLi);
+    document.getElementById('list-will-do').removeChild(clickedCheckBox.srcElement.closest('li'));
 }
