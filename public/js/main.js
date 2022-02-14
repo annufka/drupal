5623b9f3e37caa9768fd29fc50ac1aa8fbcfd2fd
Didukh <Ganna.Didukh@ua.nestle.com>
2022-02-14 10:39:20 +0200

    Что-то никак не заработает delete

--- public/js/main.js
+++ public/js/main.js
@@ -2,11 +2,10 @@
 
 let listToDo = [];
 
-window.onload = function () {
-    const saveButton = document.getElementById("btn-submit");
+const saveButton = document.getElementById("btn-submit");
 
-    if (saveButton) { saveButton.addEventListener("click", addItemToList) };
-}
+if (saveButton) { saveButton.addEventListener("click", addItemToList) };
+
 
 function addItemToList() {
     let user_input = document.getElementById("user-input");
@@ -16,20 +15,20 @@ function addItemToList() {
 
 function addItemToPage(lastItem) {
     let listInPage = document.getElementsByClassName('list-will-to-do');
-    let newLi = document.createElement('div');
-    newLi.innerHTML = `<li>
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckIndeterminate">
+    let newLi = document.createElement('li');
+    newLi.innerHTML = `<input class="form-check-input" type="checkbox" value="" id="flexCheckIndeterminate">
                         ${lastItem}
-                        <button id="btn-delete"><i class="fa fa-trash-o" style="font-size:1pem"></i></button>
-                        </li>`;
+                        <button id="btn-delete" class="btn"><i class="fa fa-trash-o" style="font-size:1pem"></i></button>`;
     listInPage[0].appendChild(newLi);
 
-    // const deleteButton =document.getElementById("btn-delete");
-    if (newLi) { newLi.addEventListener("click", deleteItem) };
+    // const deleteButton = document.getElementById("btn-delete");
+    // if (newLi) { newLi.addEventListener("click", deleteItem) };
+    newLi.addEventListener("click", deleteItem);
 }
+
 
 function deleteItem() {
     let deleteBtn = document.getElementById("btn-delete");
     deleteBtn.parentElement.remove();
+    // deleteBtn.closest('li').remove();
 }
-
